2f2a7969aa2bae974647f978e00451aaf8f83e7a
Xianyx <pushkar44320@gmail.com>
2024-04-04 14:42:28 +0530

    halfway done

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
-    private InputManager inputManager;
+    // private InputManager inputManager;
 
     private void Start() {
         
