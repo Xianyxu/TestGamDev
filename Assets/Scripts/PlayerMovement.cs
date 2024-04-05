57db367cf51c34cb7a77d7439c4a712b86259719
Xianyx <pushkar44320@gmail.com>
2024-04-05 14:39:07 +0530

    Got the player moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform orientation;
     [SerializeField] private float groundDrag;
-    [SerializeField] private float forceMultiplier;
+    [SerializeField] private float movementForceMultiplier;
     [SerializeField] private float jumpForce;
     [SerializeField] private float jumpCooldown;
     [SerializeField] private float airMultiplier;
@@ -25,6 +25,9 @@ public class PlayerMovement : MonoBehaviour
     private bool readyToJump;
     private Rigidbody rb;
 
+    [Header("Keybindings")]
+    private KeyCode jumpKey = KeyCode.Space;
+
 
     void Start()
     {
@@ -34,8 +37,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        float extraDistaceRaycast = 0.2f;
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * ConstantClass.floatHalf + extraDistaceRaycast, whatIsGround);
+        float extraDistaceToRaycast = 0.2f;
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * ConstantClass.floatHalf + extraDistaceToRaycast, whatIsGround);
 
         MyInput();
         SpeedControl();
@@ -59,31 +62,54 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw(ConstantClass.HORIZONTAL);
         verticalInput = Input.GetAxisRaw(ConstantClass.VERTICAL);
+
+        if (Input.GetKey(jumpKey) && grounded && readyToJump)
+        {
+            readyToJump = false;
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * forceMultiplier, ForceMode.Force);
-    }
 
-    private void SpeedControl(){
-        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementForceMultiplier, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementForceMultiplier * airMultiplier, ForceMode.Force);
 
-        if(flatVelocity.magnitude > moveSpeed){
-            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
         }
     }
 
-    private void Jump(){
+    private void SpeedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        Debug.Log(flatVelocity);
+
+        if (flatVelocity.magnitude > moveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+            Debug.Log(limitedVelocity);
+        }
+    }
+
+    private void Jump()
+    {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
-    private void ResetJump(){
+    private void ResetJump()
+    {
         readyToJump = true;
     }
 }
