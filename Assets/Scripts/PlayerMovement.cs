5afd4cd148af20e4173bc3379abcd50174888c29
Xianyx <pushkar44320@gmail.com>
2024-04-09 22:51:33 +0530

    Basic Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,5 +5,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform orientation;
+
+
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 moveDirection;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw(ConstantClass.HORIZONTAL);
+        verticalInput = Input.GetAxisRaw(ConstantClass.VERTICAL);
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        float forceMultiplier = 10f;
+        rb.AddForce(moveDirection.normalized * moveSpeed * forceMultiplier, ForceMode.Force);
+    }
 }
