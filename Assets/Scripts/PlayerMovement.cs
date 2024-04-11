2900dea4dd515f2ea81a74a995cc8c6370eee132
Xianyx <pushkar44320@gmail.com>
2024-04-11 22:13:19 +0530

    basic controller rig

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,13 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform orientation;
+    [SerializeField] private float groundDrag;
+
+
+    [Header("Ground Check")]
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool grounded;
 
 
     private float horizontalInput;
@@ -23,6 +30,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        // ground Check
+        float raycastDeltaAdd = .2f;
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * ConstantClass.floatHalf + raycastDeltaAdd, whatIsGround);
+
+
+        // handle drag
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+        
+        
+
         MyInput();
     }
 
