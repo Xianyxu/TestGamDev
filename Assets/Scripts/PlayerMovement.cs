e730dd4286dabb5a8994d456e966eb53924d1521
Xianyx <pushkar44320@gmail.com>
2024-04-14 05:07:21 +0530

    jump function added but not working properly
    sometimes it works and sometimes it doesn't. some issue with the  raycasting and all

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using GameDev1.Assets.Scripts.Code;
@@ -9,12 +10,21 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform orientation;
     [SerializeField] private float groundDrag;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private float airMultiplier;
+    [SerializeField] private float forceMultiplier;
+    private bool readyToJump;
 
 
     [Header("Ground Check")]
     [SerializeField] private float playerHeight;
     [SerializeField] private LayerMask whatIsGround;
     private bool grounded;
+
+
+    [Header("KeyBindings")]
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
 
     private float horizontalInput;
@@ -32,9 +42,11 @@ public class PlayerMovement : MonoBehaviour
     {
         // ground Check
         float raycastDeltaAdd = .2f;
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * ConstantClass.floatHalf + raycastDeltaAdd, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down,
+                             playerHeight * ConstantClass.floatHalf + raycastDeltaAdd, whatIsGround);
+        Debug.Log("grounded status after raycast:  " + grounded + " MaxDistance: " + playerHeight * ConstantClass.floatHalf + raycastDeltaAdd);
 
-
+        Console.WriteLine("new LIne");
         // handle drag
         if (grounded)
         {
@@ -44,10 +56,11 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.drag = 0;
         }
-        
-        
+
+
 
         MyInput();
+        SpeedControl();
     }
 
     private void FixedUpdate()
@@ -59,13 +72,61 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw(ConstantClass.HORIZONTAL);
         verticalInput = Input.GetAxisRaw(ConstantClass.VERTICAL);
+
+        
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+        
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        float forceMultiplier = 10f;
-        rb.AddForce(moveDirection.normalized * moveSpeed * forceMultiplier, ForceMode.Force);
+
+
+        // on ground
+        if (grounded)
+        {
+
+            rb.AddForce(moveDirection.normalized * moveSpeed * forceMultiplier, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+
+            rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Force);
+
+        }
+
+
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity
+        if (flatVelocity.magnitude > moveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
